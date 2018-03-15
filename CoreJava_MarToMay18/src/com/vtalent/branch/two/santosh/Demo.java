f83fb9e2a6088622b8299524b6a161ba4ebf70be
santhu453 <santhoshyr11@gmail.com>
2018-03-15 11:19:37 +0530

    created

--- CoreJava_MarToMay18/src/com/vtalent/branch/two/santosh/Demo.java
+++ CoreJava_MarToMay18/src/com/vtalent/branch/two/santosh/Demo.java
@@ -16,7 +16,7 @@ public class Demo {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub,
         Demo d1=new Demo();
         System.out.println(d1.i);
         System.out.println(d1.j);
