c043a5974f518a430a54f42d6406723f990a58f6
akhil3077 <akhil.vtalent@gmail.com>
2018-03-29 10:22:12 +0530

    scanner

---
+++ CoreJava_MarToMay18/src/com/vtalent/branch/two/akhil/CommandExample.java
@@ -0,0 +1,25 @@
+package com.vtalent.branch.two.akhil;
+
+import java.util.Scanner;
+
+public class CommandExample {
+	public static void main(String[] args) {
+		
+	
+Scanner ak = new Scanner(System.in);
+
+System.out.println("enter your name");
+
+String name = ak.next();
+
+System.out.println("enter phone number");
+
+int number = ak.nextInt();
+
+System.out.println(name +" "+ number);
+
+
+	
+	
+}
+}
