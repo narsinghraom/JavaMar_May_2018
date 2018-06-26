a2ee57998303916452e78d4b7a1364075dc0c106
yerramsowmya <yerramsowmya1994@gmail.com>
2018-06-26 18:43:04 +0530

    Factorial
    Test the Factorial problem.

---
+++ CoreJava_MarToMay18/src/com/vtalent/brach/sowmya/Factorial.java
@@ -0,0 +1,18 @@
+package com.vtalent.brach.sowmya;
+
+public class Factorial {
+
+	
+
+		public static void main(String args[])
+		{
+			int i, fact=1;
+					int num=6;
+					for(i=1;i<=num;i++)
+					{
+						fact=fact*i;
+		}
+		System.out.println("enter the value "+num+" is:" +fact);
+	}
+
+}
