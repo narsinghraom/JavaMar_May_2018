c2cd1bb83c318eb09f18bef00271f6f2b12f8822
sheriswathi <40557416+sheriswathi@users.noreply.github.com>
2018-06-26 12:43:58 +0530

    swathi
     test palindrome

---
+++ CoreJava_MarToMay18/src/Palindrome.java
@@ -0,0 +1,26 @@
+
+ class Palindrome {
+	 public static void main(String args[])
+	 {
+	 String original="mom",reverse=" ";
+	 
+	 for(int i=(original.length()-1);i>=0;i--)
+	 {
+		 reverse=reverse+original.charAt(i);
+	 }
+	 if(original.equals(reverse))
+	 {
+	 
+		 System.out.println("string is palindrome");
+	 }
+	 
+	 else
+	 
+		 System.out.println("string is not a palindrome"); 
+	 
+	 
+ }
+	 }
+	 
+
+
