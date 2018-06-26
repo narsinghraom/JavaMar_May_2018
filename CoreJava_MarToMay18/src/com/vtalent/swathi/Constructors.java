bb9b03e4d7096f255844e8a27844e71b68e88803
sheriswathi <40557416+sheriswathi@users.noreply.github.com>
2018-06-26 21:43:29 +0530

    constructors
    code for constructor

---
+++ CoreJava_MarToMay18/src/com/vtalent/swathi/Constructors.java
@@ -0,0 +1,43 @@
+package com.vtalent.swathi;
+
+
+
+
+
+class Constructors{
+		 int i,j;
+		 Constructors()
+		 {
+			 i=5;j=7;
+		 }
+		 Constructors(int i)
+		 {
+			 this.i=i+i;
+			 this.j=i*i;
+		 }
+		 Constructors(int i,int j)
+		 {
+			 this.i=i+j;
+			 this.j=i*j;
+		 }
+		 public void function()
+		 {
+			 System.out.println(i);
+			 System.out.println(j);
+			 
+		 }
+		 public static void main(String args[])
+		 {
+			 Constructors c=new Constructors();
+			 c.function();
+			 Constructors c1=new Constructors(3);
+			 c1.function();
+			 Constructors c2=new Constructors(3,3);
+			 c2.function();
+		 }
+		 }
+
+
+
+
+
