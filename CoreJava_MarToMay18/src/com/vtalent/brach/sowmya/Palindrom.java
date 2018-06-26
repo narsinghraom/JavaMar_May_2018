38275218dce8978879e7ccf2fe2b406f7ae5b02b
yerramsowmya <yerramsowmya1994@gmail.com>
2018-06-26 18:37:22 +0530

    Palindrom
    Test given number is palindrom number or not

---
+++ CoreJava_MarToMay18/src/com/vtalent/brach/sowmya/Palindrom.java
@@ -0,0 +1,27 @@
+package com.vtalent.brach.sowmya;
+
+public class Palindrom {
+	
+		public static void palindrom (String value)
+		{
+	    String reverseVal=""; 
+	    for(int i = value.length() - 1;i >= 0;i--)
+	    {
+	    reverseVal =reverseVal+value.charAt(i);
+	    }
+	    if(value.equals(reverseVal))
+	    {
+	    	System.out.println("palindrom");
+	    }
+	    else
+	    {
+	    	System.out.println("not a palindrom");
+	    }
+		}
+		public static void main(String args[ ])
+		{
+			palindrom("malayalam");
+		}
+	}
+
+
