076a1bfd09e39b18fbb8f6f2326525a82868d021
TharunJakinapelly <tharunjakinapelly26@gmail.com>
2018-06-26 12:15:58 +0530

    Hi
    TextFile

--- CoreJava_MarToMay18/src/com/vtalent/branch/one/karthik/Tharun.java
+++ CoreJava_MarToMay18/src/com/vtalent/branch/one/karthik/Tharun.java
@@ -2,7 +2,8 @@ package com.vtalent.branch.one.karthik;
 
 public class Tharun {
    public static void main(String[] args) {
-	
+	   System.out.println("Hi");
 	   System.out.println("Tharun");
+	   
    }
 }
