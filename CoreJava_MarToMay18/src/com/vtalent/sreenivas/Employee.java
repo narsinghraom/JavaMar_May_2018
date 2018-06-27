48c606572a99cf8909442a3bbf0c277cb9b32818
SreenivasaReddy12 <40225605+SreenivasaReddy12@users.noreply.github.com>
2018-06-27 11:12:36 +0530

    my class2
    test it

--- CoreJava_MarToMay18/src/com/vtalent/sreenivas/Employee.java
+++ CoreJava_MarToMay18/src/com/vtalent/sreenivas/Employee.java
@@ -7,20 +7,20 @@ long emobile;
 static Employee[] emparray=new Employee[2];
 public static void insertData()
 {
-	for(int i=0;i<emparray.length-1;i++)
+	for(int i=0;i<=emparray.length-1;i++)
 	{
 		Employee emp=new Employee();
 		emp.empid=50+i;
 		emp.empsal=20000*(5*i);
 		emp.emobile=888633823+(i*2);
-		emparray[i]=emp;
+		emparray[1]=emp;
 	}
 }
 public static void displayData()
 {
-	for(int i=0;i<emparray.length-1;i++)
+	for(int i=0;i<=emparray.length-1;i++)
 	{
-		Employee emp=(Employee)emparray[i];
+		Employee emp=(Employee)emparray[1];
 		System.out.println(emp.empid+" "+emp.empsal+" "+emp.emobile);
 	}
 }
