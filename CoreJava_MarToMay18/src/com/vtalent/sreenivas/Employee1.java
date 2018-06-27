44556dc3e1bbbc1f44805f29991a16d604eafe99
SreenivasaReddy12 <40225605+SreenivasaReddy12@users.noreply.github.com>
2018-06-27 11:03:17 +0530

    My class1
    Test it

--- CoreJava_MarToMay18/src/com/vtalent/sreenivas/Employee1.java
+++ CoreJava_MarToMay18/src/com/vtalent/sreenivas/Employee1.java
@@ -8,33 +8,30 @@ public class Employee1
 	String ename;
 	double esal;
 	int deptno;
-	public void employeedata()
-	{
-		Scanner s=new Scanner(System.in);
-		System.out.println("enter the employee id");
-		int empid=s.nextInt();
-		System.out.println("enter the employee name");
-		String ename=s.next();
-		System.out.println("enter the employee esal");
-		double esal=s.nextDouble();
-		System.out.println("enter the employee deptno");
-		int deptno=s.nextInt();
-	}
-	public void display()
-	{
-		System.out.println("the employee id is="+empid);
-		System.out.println("enter the employee name="+ename);
-		System.out.println("enter the employee esal="+esal);
-		System.out.println("enter the employee deptno="+deptno);
-		
-	}
+Employee1()
+{
+	empid=101;
+	ename="sreenu";
+	esal=25000.00;
+	deptno=10;
+	
+}
+public void display()
+{
+	System.out.println("the emp id is="+empid);
+	System.out.println("the emp name is="+ename);
+	System.out.println("the emp sal is="+esal);
+	System.out.println("the emp deptno is="+deptno);
+}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Employee1 e=new Employee1();
-		e.employeedata();
 		e.display();
-
+		/*e.employeedata();
+		e.display();
+*/
 	}
 
 }
