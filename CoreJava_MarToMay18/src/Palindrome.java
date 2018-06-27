eb95342c72e515cf64ad16e88764530ad1c34d74
TharunJakinapelly <tharunjakinapelly26@gmail.com>
2018-06-27 11:34:10 +0530

    Employee
    Employee Details

--- CoreJava_MarToMay18/src/Palindrome.java
+++ CoreJava_MarToMay18/src/Palindrome.java
@@ -3,7 +3,7 @@
 	 public static void main(String args[])
 	 {
 	 String original="mom",reverse=" ";
-	 
+	  
 	 for(int i=(original.length()-1);i>=0;i--)
 	 {
 		 reverse=reverse+original.charAt(i);
---
+++ CoreJava_MarToMay18/src/com/tharun/Employee.java
@@ -0,0 +1,46 @@
+package com.tharun;
+
+import java.util.Scanner;
+
+public class Employee {
+  int eId;
+  double eSalary;
+  long eMobile;
+  static Employee[] empArray = new Employee[4];
+  public static void insertData()
+  {
+	  for(int i=0;i<=empArray.length-1;i++)
+	  {
+		  Employee emp = new Employee();
+		  emp.eId=100+i;
+		  emp.eSalary=2500*(10+i);
+		  emp.eMobile=970042372+(i*2);
+		  empArray[i]=emp;
+	  }
+  }
+  public static void displayEmp()
+  {
+	  for(int i=0;i<=empArray.length-1;i++)
+	  {
+		  Employee emp = (Employee)empArray[i];
+		  System.out.println("Id="+emp.eId+"Salary="+emp.eSalary+"Mobile="+emp.eMobile);
+		  
+	  }
+  }
+  public static void displayOneEmp()
+  {
+	  Scanner sc = new Scanner(System.in);
+	  int temp = sc.nextInt();
+	  for(int i=0;i<=empArray.length-1;i++) {
+	  Employee emp = (Employee)empArray[i];
+	 	   if(emp.eId==temp){
+		 System.out.println(empArray[i]);
+	   }
+	 }
+  }
+  public static void main(String[] args){
+	  insertData();
+	  displayEmp();
+	  displayOneEmp();
+  }
+}
