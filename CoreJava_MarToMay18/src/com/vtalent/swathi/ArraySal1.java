c79e3b03592c22a29a06515e47c3ef0c0aa3f45a
sheriswathi <40557416+sheriswathi@users.noreply.github.com>
2018-07-03 09:46:14 +0530

    my chanfes

--- CoreJava_MarToMay18/src/com/vtalent/swathi/ArraySal1.java
+++ CoreJava_MarToMay18/src/com/vtalent/swathi/ArraySal1.java
@@ -27,34 +27,50 @@ package src.com.vtalent.swathi;
 	 				 {
 	 					 for(int i=0;i<=empArray.length-1;i++)
 	 					 {
+	 						 if(null!=empArray[i])
+	 						 {
 	 						 ArraySal1 emp=(ArraySal1)empArray[i];
 	 				 System.out.println("id="+emp.eid+"salary="+emp.esalary+"mobile="+emp.emobile);
 	 				 
-	 					 
+	 						 }
 	 					 }
 	 		 }
 	 				 public static void displayOne()
 	 				 {
+
+ 						 Scanner sc=new Scanner(System.in);
+	 					 System.out.println("enter the input");
+	 					 int temp=sc.nextInt();
+	 				
 	 					 for(int i=0;i<=empArray.length-1;i++)
 	 					 {
-	 						 Scanner sc=new Scanner(System.in);
-		 					 System.out.println("enter the input");
-		 					 int temp=sc.nextInt();
-		 					
-	 					 ArraySal1 emp=(ArraySal1)empArray[i];
+
+	 					 	 ArraySal1 emp=(ArraySal1)empArray[i];
+	 					
 	 					 if(temp==(emp.eid))
 						 {
 							 System.out.println(emp.eid+" "+emp.esalary+" "+emp.emobile);
 							 System.out.println("enter the amount to update the salary");
-							 Scanner sc1=new Scanner(System.in);
-							 int s=sc1.nextInt();
+							 
+							 int s=sc.nextInt();
 							 emp.esalary=emp.esalary+s;
+						//empArray[i]=emp;
+							 empArray[i]=null;
+							 
 							  System.out.println(emp.eid+" "+emp.esalary+" "+emp.emobile);
+							  break;
 						 }
+	 					 else
+	 					 
+	 						 System.out.println("please enter the input again");
+	 					 
+	 					 
+	 					 }
+	 					 
 	 					 
 	 				 }
 
-	 		}
+	 		
 	 				 public static void main(String[] args)
 	 				 {
 	 					 insertData();
