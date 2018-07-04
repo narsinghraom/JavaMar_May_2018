b84c7ec9ccfd8b560e2175ea8a1745fdef0e9c82
SreenivasaReddy12 <40225605+SreenivasaReddy12@users.noreply.github.com>
2018-07-04 12:26:13 +0530

    Encapsulation
    Test it code

---
+++ CoreJava_MarToMay18/com/vtalent/sreenivas/TestEmployeeEncapsulation.java
@@ -0,0 +1,28 @@
+package com.vtalent.sreenivas;
+
+import java.util.Scanner;
+
+public class TestEmployeeEncapsulation 
+{
+	private int empid;
+	private double salary;
+	private long mobile;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public long getMobile() {
+		return mobile;
+	}
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+}
