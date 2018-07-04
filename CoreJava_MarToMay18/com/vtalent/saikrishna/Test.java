22207967f4ce1493a597b9b3ccad80c1c341dded
saikrishna <saikrishna6033.vtalent@gmail.com>
2018-07-04 11:55:41 +0530

    EncapsulationPrg

--- CoreJava_MarToMay18/com/vtalent/saikrishna/Test.java
+++ CoreJava_MarToMay18/com/vtalent/saikrishna/Test.java
@@ -1,16 +1,35 @@
 package com.vtalent.saikrishna;
 
-public class Test {
 
-	public static void main(String[] args) 
+public class Test 
+{
+	 static ArrayEmployee[] empArray=new ArrayEmployee[4]; 
+
+	public static void insertData()
 	{
-		EncapsulationPrg ep=new EncapsulationPrg();
-		ep.setEmpid(1122);
-		ep.setEmpName("sai");
-		ep.setEmpsal(5000);
-		
-		System.out.println("employee id="+ep.getEmpid());
-		System.out.println("employee name="+ep.getEmpName());
-		System.out.println("employee salary="+ep.getEmpsal());
+		for(int i=0;i<=empArray.length-1;i++)
+		{
+			ArrayEmployee e1=new ArrayEmployee();
+			e1.setEmpid(1000+i);
+			e1.setEmobile(93939+(i*i));
+			e1.setEsal(5000+(i*2));
+			empArray[i]=e1;
+			//System.out.println("data inserted");
+		}
+	}
+	
+	public static void dispAllEmps()
+	{
+		for(int i=0;i<=empArray.length-1;i++)
+		{
+			ArrayEmployee e=(ArrayEmployee)empArray[i];
+			System.out.println("id="+e.getEmpid()+"sal="+e.getEsal()+" number="+e.getEmobile());
+		}
+	}
+
+  public static void main(String[] args) 
+	{
+		insertData();
+		dispAllEmps();
 	}
 }
