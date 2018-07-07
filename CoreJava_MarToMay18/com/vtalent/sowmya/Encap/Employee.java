b65958ad6aa7c48422f9f6b32004be37863423ac
yerramsowmya <yerramsowmya1994@gmail.com>
2018-07-07 13:08:28 +0530

    Encapsulation
    Encapulation using Arrays

---
+++ CoreJava_MarToMay18/com/vtalent/sowmya/Encap/Employee.java
@@ -0,0 +1,21 @@
+package com.vtalent.sowmya.Encap;
+
+public class Employee {
+	private int empId;
+	private double empSalary;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public double getEmpSalary() {
+		return empSalary;
+	}
+	public void setEmpSalary(double empSalary) {
+		this.empSalary = empSalary;
+	}
+	
+
+	
+}
