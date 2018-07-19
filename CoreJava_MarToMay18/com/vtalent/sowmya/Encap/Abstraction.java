00cb6ead0438a989a7c182d1b608cb7ef7441693
yerramsowmya <yerramsowmya1994@gmail.com>
2018-07-19 10:44:44 +0530

    test
    hii

---
+++ CoreJava_MarToMay18/com/vtalent/sowmya/Encap/Abstraction.java
@@ -0,0 +1,5 @@
+package com.vtalent.sowmya.Encap;
+
+public class Abstraction {
+
+}
---
+++ CoreJava_MarToMay18/com/vtalent/sowmya/Encap/String.java
@@ -0,0 +1,14 @@
+package com.vtalent.sowmya.Encap;
+
+public class String1 {
+	public static void main(String args[])
+	{
+		String s1=new String("Abc");
+		System.out.println(s1);
+//		String s2="Abc";
+//		String s3="Abc";
+		
+		
+	}
+
+}
